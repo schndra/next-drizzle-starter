cde0fa0e18688d0bc123f80aaf10d79c389c41d2
schndra <ardnihc@gmail.com>
2024-08-27 11:54:00 +0530

    feat: add index.ts to query data

---
+++ src/db/index.ts
@@ -0,0 +1,24 @@
+import * as schema from "./schema";
+import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+declare global {
+  // eslint-disable-next-line no-var -- only var works here
+  var database: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let database: PostgresJsDatabase<typeof schema>;
+let pg: ReturnType<typeof postgres>;
+
+if (process.env.NODE_ENV === "production") {
+  pg = postgres(process.env.DATABASE_URL!);
+  database = drizzle(pg, { schema });
+} else {
+  if (!global.database) {
+    pg = postgres(process.env.DATABASE_URL!);
+    global.database = drizzle(pg, { schema });
+  }
+  database = global.database;
+}
+
+export { database, pg };
